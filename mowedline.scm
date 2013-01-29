68eddedab6885002c20bafc7a637478ca8b9ab59
John Foerch <jjfoerch@earthlink.net>
2013-01-29 14:50:18 -0500

    legalese

--- mowedline.scm
+++ mowedline.scm
@@ -1,6 +1,6 @@
 
 ;; This file is part of mowedline.
-;; Copyright (C) 2011  John J. Foerch
+;; Copyright (C) 2011-2013  John J. Foerch
 ;;
 ;; mowedline is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
