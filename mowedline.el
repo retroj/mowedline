a9c3c8e0f8c15f8ce1f67fbda85c61f155732269
John Foerch <jjfoerch@earthlink.net>
2016-11-14 08:43:03 -0500

    mowedline.el: update file version and date

--- mowedline.el
+++ mowedline.el
@@ -4,8 +4,8 @@
 ;; Copyright (C) 2011-2015  John J. Foerch
 
 ;; Author: John Foerch <jjfoerch@earthlink.net>
-;; Version: 0.3
-;; Date: 2015-06-01
+;; Version: 0.4
+;; Date: 2016-11-10
 
 ;; mowedline is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
